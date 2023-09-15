254cd41d8413822d301f1a8a23867b04f467fa46
CharlesKleeven <charles.kleeven@gmail.com>
2023-09-15 15:04:38 -0700

    Updates

---
+++ precache-manifest.d3c5b4707df093a6829f3061ff803316.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "55ee3d3be72672463dd5",
+    "url": "/portfolio/static/css/main.b49237c5.chunk.css"
+  },
+  {
+    "revision": "55ee3d3be72672463dd5",
+    "url": "/portfolio/static/js/main.2fbddaa6.chunk.js"
+  },
+  {
+    "revision": "810a7ef96837d64b11ab",
+    "url": "/portfolio/static/js/runtime~main.af4327ea.js"
+  },
+  {
+    "revision": "d71679eecb3448795512",
+    "url": "/portfolio/static/js/2.cbcf0536.chunk.js"
+  },
+  {
+    "revision": "629666658164b43f59b6",
+    "url": "/portfolio/static/js/3.45bd1f93.chunk.js"
+  },
+  {
+    "revision": "9b3e7175765db7efcc33767ae8ca812c",
+    "url": "/portfolio/static/media/rhonda.9b3e7175.png"
+  },
+  {
+    "revision": "dc32a3eda204576e398b5402bdf22e03",
+    "url": "/portfolio/static/media/leafboy.dc32a3ed.png"
+  },
+  {
+    "revision": "93f1f9d0ba36e3a9cce2d29522f17ca8",
+    "url": "/portfolio/static/media/shhh.93f1f9d0.png"
+  },
+  {
+    "revision": "bf49098185e29f848c000ce26bae4a12",
+    "url": "/portfolio/static/media/yukyoStation.bf490981.png"
+  },
+  {
+    "revision": "83bae99f97c63239a0943d9f31179ba4",
+    "url": "/portfolio/static/media/paper.83bae99f.png"
+  },
+  {
+    "revision": "e127bb1f0bfa1406b3449daf7fd35812",
+    "url": "/portfolio/static/media/temple.e127bb1f.png"
+  },
+  {
+    "revision": "c6b8532055b011de3ceb6dda4a2de3f5",
+    "url": "/portfolio/index.html"
+  }
+];
