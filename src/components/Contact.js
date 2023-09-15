3a877bd41a0493f5a11b49923bee6b1a3aaea6b2
CharlesKleeven <charles.kleeven@gmail.com>
2023-09-14 22:21:38 -0700

    added borders to input text and asterisks to indicate required fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,28 +17,27 @@ function Contact() {
         <form className="form-container">
             <div className="name-group">
                 <div className="form-group">
-                    <label htmlFor="firstName">First name</label>
+                    <label htmlFor="firstName">First name<span className="required-label">*</span></label>
                     <input type="text" id="firstName" name="firstName" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="lastName">Last name</label>
-                    <input type="text" id="lastName" name="lastName" unrequired />
+                    <input type="text" id="lastName" name="lastName" />
                 </div>
             </div>
 
-
             <div className="form-group">
-                <label htmlFor="email">Email</label>
+                <label htmlFor="email">Email<span className="required-label">*</span></label>
                 <input type="text" id="email" name="email" required />
             </div>
 
             <div className="form-group">
-                <label htmlFor="subject">Subject</label>
+                <label htmlFor="subject">Subject<span className="required-label">*</span></label>
                 <input type="text" id="subject" name="subject" required />
             </div>
 
             <div className="form-group">
-                <label htmlFor="message">Message</label>
+                <label htmlFor="message">Message<span className="required-label">*</span></label>
                 <textarea id="message" name="message" rows="4" required></textarea>
             </div>
 
